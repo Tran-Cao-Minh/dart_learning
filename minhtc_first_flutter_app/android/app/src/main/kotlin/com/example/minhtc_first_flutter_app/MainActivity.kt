d9b886b3b822f3a22f7d08c4c04475322ad42002
Trần Cao Minh <78601445+Tran-Cao-Minh@users.noreply.github.com>
2022-04-25 16:16:17 +0700

    update flutter projects

---
+++ minhtc_first_flutter_app/android/app/src/main/kotlin/com/example/minhtc_first_flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.minhtc_first_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ startup_minhtc/android/app/src/main/kotlin/com/example/startup_minhtc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.startup_minhtc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
